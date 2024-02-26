410d322a72fb3cd49bd1e91c5f631adde8e5e826
Martin <marekatt2004@gmail.com>
2024-02-26 10:20:37 +0100

    Added image carousel

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,33 @@
 const test = document.querySelector("div")
-
-console.log(test)
+const slideContainer = document.querySelector(".slideContainer")
+const images = [1,2,3,4,5,6]
 
 test.onclick = () => {
     console.log("click")
 }
 
+let index = 0
+
+const switchImage = (index) => {
+    setTimeout(() => {
+        console.log(index)
+        changeHtml(index)
+        if(index >= images.length - 1){
+            index = 0
+        } else{
+            index++
+        }
+        switchImage(index)
+    }, 1000)
+}
+
+const changeHtml = (index) => {
+    slideContainer.innerHTML = `
+        <div>${images[index]}</div>
+    `
+}
+
+// starts the image carousel
+changeHtml(index)
+index++
+switchImage(index)
