21c2a9029f51e5f0eea8e73efa910c77b9531dcf
Martin <marekatt2004@gmail.com>
2024-02-29 09:43:12 +0100

    Added node and express to dynamically to get all the images

---
+++ public/scripts/gallery.js
@@ -0,0 +1,44 @@
+const imageContainer = document.querySelector(".gridContainer")
+
+
+
+let images = []
+
+const getImages = async () => {
+    const response = await fetch("/images")
+    let data = await response.json()
+    //imagesTest = {... {image : await data.images[2]}} 
+    await data.images.forEach(image => {
+        images.push(image)
+    });
+    images.forEach(image => {
+        imageContainer.innerHTML += `
+            <img class="gridItem" src="./nfs/nfs/aiart/images/${image}" alt="${image.split(" - ")[1]}"></img>
+            `
+    });
+    
+    const galleryImage = document.querySelectorAll(".gridItem") 
+    console.log(galleryImage)
+    const popUpElement = document.querySelector(".popUp")
+    const popUp = (src) => {
+        popUpElement.src = src
+        popUpElement.classList.remove("popUp")
+        popUpElement.classList.add("fullScreenImage")
+    }
+    
+    galleryImage.forEach(image => {
+        image.addEventListener("click", (e) => {
+            popUp(e.target.src) // start popup
+        })
+    });
+    
+    popUpElement.addEventListener("click", (e) => { // remove popup
+        popUpElement.classList.remove("fullScreenImage")
+        popUpElement.classList.add("popUp")
+    })
+}
+
+getImages()
+
+
+
